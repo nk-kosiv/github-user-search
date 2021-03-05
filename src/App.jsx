52b45a0129b5cba195ccc459e458c9b178c9d733
Nazar Khrushch <mac15pro@Nazar-Khrushch.local>
2021-03-05 17:57:31 +0200

    instaled redux in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import gal from './images/galexy.jpg';
+import gal from "./images/galexy.jpg";
 
-import './index.scss';
+import "./index.scss";
 
 const App = () => {
-        return (
-            <div>
-                <h1>My React App!</h1>
-                <img src={gal} alt="gal"/>
-            </div>
-        );
-}
+  return (
+    <div>
+      <h1>My React App!</h1>
+      <img src={gal} alt="gal" />
+    </div>
+  );
+};
 
 export default App;
